3bf0bb56d5508efac4d6818eb377c4fc11722aeb
ggingmin <ggingbookair-ct@KYEONGMINui-MacBookAir.local>
2022-06-15 16:53:36 +0900

    first commit

---
+++ components/layout.js
@@ -0,0 +1,63 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+const name = 'ggingmin'
+export const siteTitle = 'ggingmin'
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="ggingmin"
+        />
+        <meta
+          property="og:image"
+          content="/images/ggingmin_ogimage.jpg"
+        />
+        <meta name="og:title" content={siteTitle} />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/ggingmin_mimoji.png"
+              className={utilStyles.borderCircle}
+              height={200}
+              width={200}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <Image
+                  priority
+                  src="/images/ggingmin_mimoji.png"
+                  className={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+    </div>
+  )
+}
